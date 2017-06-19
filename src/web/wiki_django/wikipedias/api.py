8fd636149a697036f090897f9a0ada080938fe9b
ripper <ricardo.pcan@gmail.com>
2017-06-19 16:48:41 -0500

    Add cath exceptions

--- src/web/wiki_django/wikipedias/api.py
+++ src/web/wiki_django/wikipedias/api.py
@@ -2,7 +2,8 @@
 from wiki_django.api.v1.routers import router
 from rest_framework.decorators import list_route
 from wiki_django.core.api.viewsets import GenericViewSet
-from subprocess import call
+from rest_framework import status
+from subprocess import check_call, CalledProcessError
 import random
 from django.http import HttpResponse
 
@@ -12,21 +13,11 @@ class WikipediaViewSet(
     @list_route(methods=['GET'])
     def get_pdf(self, request, *args, **kwargs):
         """
-        Return events list.
+        Return wikipedia pdf by title.
         ---
-        response_serializer: serializers.EventSerializer
         parameters:
-            - name: fromdate
-              description: initial date. Format YYYY-MM-DD
-        print query_params
-              paramType: query
-              type: string
-            - name: place
-              description: filter by place id
-              paramType: query
-              type: integer
-            - name: todate
-              description: end date. Format YYYY-MM-DD
+            - name: title
+              description: filter by title id
               paramType: query
               type: string
         responseMessages:
@@ -48,12 +39,20 @@ class WikipediaViewSet(
         hash = random.getrandbits(16)
         hash = str(hash)
 
-        call(['mw-zip', '-c', ':es', '-o', '/tmp/'+hash+'.zip', title])
-        call(['mw-render', '-c', '/tmp/'+hash+'.zip', '-o', '/tmp/'+hash+'.pdf', '-w', 'rl'])
+        try:
+            check_call(['mw-zip', '-c', ':es', '-o', '/tmp/'+hash+'.zip', title])
+            check_call(['mw-render', '-c', '/tmp/'+hash+'.zip', '-o', '/tmp/'+hash+'.pdf', '-w', 'rl'])
+        except CalledProcessError as error:
+            response = HttpResponse(
+                error.strerror,
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+            return response
 
         with open('/tmp/'+hash+'.pdf', 'rb') as pdf:
             response = HttpResponse(pdf.read(),content_type='application/pdf')
             response['Content-Disposition'] = 'filename='+hash+'.pdf'
+            response['status'] = status.HTTP_200_OK
             return response
 
 
